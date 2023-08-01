3cade2f91094a4d2d8187ec84c631b88361d2ecb
User <yuri.ogorodnik@mail.ru>
2023-08-01 20:03:22 +0300

    Update for homework-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees (
+   employee_id serial PRIMARY KEY,
+   first_name varchar(50),
+   last_name varchar(50),
+   title varchar(50),
+   birth_date date,
+   notes text
+);
+
+
+CREATE TABLE customers (
+    customer_id varchar(100) UNIQUE,
+    company_name varchar(100),
+    contact_name varchar(100)
+);
+
+
+CREATE TABLE orders (
+  order_id int,
+  customer_id varchar(100) REFERENCES customers (customer_id),
+  employee_id int REFERENCES employees (employee_id),
+  order_date date,
+  ship_city VARCHAR(100)
+)
+
+
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,33 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+
+# Подключение к базе данных
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='ogr25men')
+cursor = conn.cursor()
+
+# Имя файла и таблицы для заполнения данными
+files = {
+    "employees": "north_data/employees_data.csv",
+    "customers": "north_data/customers_data.csv",
+    "orders": "north_data/orders_data.csv"
+}
+
+# Заполнение таблиц данными из файлов
+for table_name, file_path in files.items():
+    with open(file_path, "r") as file:
+        csv_data = csv.reader(file, delimiter=",")
+        next(csv_data)  # Пропускаем заголовок CSV-файла
+
+        for row in csv_data:
+            # Формируем SQL-запрос для вставки данных в таблицу
+            sql = f"INSERT INTO {table_name} VALUES ({','.join(['%s'] * len(row))})"
+            cursor.execute(sql, row)
+
+# Фиксируем изменения и закрываем соединение
+conn.commit()
+cursor.close()
+conn.close()
+
+print("Данные успешно загружены в таблицы.")
